019442990c0e08233fa9820475200de316b5af75
Vera Spiridonova <ver-spiridonova@yandex.ru>
2025-07-31 14:59:14 +0300

    Configure Russian localization and add translations

--- orders/settings.py
+++ orders/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -161,5 +162,15 @@ REST_FRAMEWORK = {
     ),
 
 }
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale'),
+]
+
+# optional
+LANGUAGES = [
+    ('en', 'English'),
+    ('ru', 'Russian'),
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
